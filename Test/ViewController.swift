52250971bfaccae08d33f0a7aaba8f508a2ef2c0
GUNWOO1207 <s22045@gsm.hs.kr>
2022-12-26 19:48:36 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by GSM02 on 2022/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
